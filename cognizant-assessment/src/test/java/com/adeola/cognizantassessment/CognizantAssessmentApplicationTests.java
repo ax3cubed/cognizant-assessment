9c78a9ddefd43a603875cb1e421a526d7926ac06
Akinwole <ajunior488@gmail.com>
2022-02-09 23:28:16 +0100

    microservice setup for the assessment including base dependencies

---
+++ cognizant-assessment/src/test/java/com/adeola/cognizantassessment/CognizantAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adeola.cognizantassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognizantAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
